b212e90ebcdb1efe243c85d845e4cbf53607000b
Misha Dzhuzha <dzhuzhamichael@gmail.com>
2025-07-15 02:46:24 +0300

    run in docker env finished

--- src/main/java/test/task/schoolregistry/SchoolRegistryApplication.java
+++ src/main/java/test/task/schoolregistry/SchoolRegistryApplication.java
@@ -1,13 +1,23 @@
 package test.task.schoolregistry;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SchoolRegistryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SchoolRegistryApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner onStartup() {
+        return args -> {
+            System.out.println("Усі сервіси запущено!");
+            System.out.println("Відкрийте браузер: http://localhost:3000");
+        };
     }
 
 }
